b6a23d5f1c10b2a4c223bf3d0bbed60723752020
DmitryKress <54477341+DmitryKress@users.noreply.github.com>
2019-09-28 13:46:58 +0300

    1

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+var helloworld = "Hello World ";
+var today = "Sat";
+
+console.log(typeof today);
+
+if (today == "Sat") {
+  console.log("Today is saturday");
+} else {
+  console.log("whatever");
+}
+
+console.log(helloworld);
+console.log(helloworld + "my first app");
+console.log(helloworld + "my second app");
+console.log(helloworld + "my third app");

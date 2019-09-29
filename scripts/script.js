606f3ca884a7c1ce55b4368559f2a665734dca06
DmitryKress <dmitry_job@bk.ru>
2019-09-29 21:31:50 +0300

    3

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,10 @@
-var product = prompt("What do you like to order?    ");
-console.log("You choosed", product);
+var myCurrentAge = "MyCurentAge?";
+var today = "16";
 
-switch (product) {
-  case "Orange":
-    console.log("Oranges are 0,59pounds");
-    break;
-  case "Bananas":
-    console.log("Bananas are 1,23pounds");
-    break;
-  case "Cheries":
-    console.log("Cheries are 4,56pounds");
-    break;
-  default:
-    console.log("Sorry " + product + " doesn't exist");
+console.log(typeof today);
+
+if (today === "25") {
+  alert("I am over 25 years old");
+} else {
+  alert("I am under 25");
 }
-
-console.log(1 + 2);
---
+++ scripts2/script.js
@@ -0,0 +1,3 @@
+var myCurrentAge = 26;
+
+myCurrentAge >= 16 ? alert("I am over 25 years old") : alert("I am under 25");

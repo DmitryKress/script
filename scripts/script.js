3b7e7fe9184357d3d9cc1e96e634b950f316a009
DmitryKress <54477341+DmitryKress@users.noreply.github.com>
2019-09-29 13:43:23 +0300

    2

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,18 @@
-var helloworld = "Hello World ";
-var today = "Sat";
+var product = prompt("What do you like to order?    ");
+console.log("You choosed", product);
 
-console.log(typeof today);
-
-if (today == "Sat") {
-  console.log("Today is saturday");
-} else {
-  console.log("whatever");
+switch (product) {
+  case "Orange":
+    console.log("Oranges are 0,59pounds");
+    break;
+  case "Bananas":
+    console.log("Bananas are 1,23pounds");
+    break;
+  case "Cheries":
+    console.log("Cheries are 4,56pounds");
+    break;
+  default:
+    console.log("Sorry " + product + " doesn't exist");
 }
 
-console.log(helloworld);
-console.log(helloworld + "my first app");
-console.log(helloworld + "my second app");
-console.log(helloworld + "my third app");
+console.log(1 + 2);
